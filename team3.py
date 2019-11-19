718fb9e882f7c4edb791169dc057636e1493079b
sanjith <550051@my.cuhsd.org>
2019-11-19 13:29:50 -0800

    Captions
    Added captions to all our scenarios.

--- team3.py
+++ team3.py
@@ -26,8 +26,28 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+
+      # This is our function for the project.
+    def move(my_history, their_history, my_score, their_score):
+     turns_value = len(my_history)
+
+    # This is our first scenario that acts on the first turn.
+    if len(my_history) == 0:
+        return 'b' 
+        
+    #  This is our second scenario that acts if we are losing after 20 turns. 
+    elif turns_value >= 20:
+        return 'b'
+        
+    #  This is our third scenario that acts if our score is at or below -2000.  
+    elif  my_score <= -2000:
+        return 'b'
+        
+    #  This is our fourth scenario that acts if all the other scenarios don't work.   
+    else: 
+	return their_history[-1]
+
+  
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
