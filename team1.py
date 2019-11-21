c0ed1b5ee83f65c7a4d067ba4c1d02bee12992a2
trent-algea <54417741+trent-algea@users.noreply.github.com>
2019-11-21 13:29:00 -0800

    Update team1.py

--- team1.py
+++ team1.py
@@ -6,28 +6,28 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+team_name = '3/4 Asian' # Only 10 chars displayed.
+strategy_name = 'Basic Bois'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
     #Scenario #1: First Turn Collude
     if len(my_history)==0: #It's the first round; collude.
         return 'c'
-    elif my_history[-1]=='c' and their_history[-1]=='b':
+    elif my_history[-1]=='c' and their_history[-1]=='b': #If my history is c and their history is b, then return with b. 
         return 'b' 
     else:
-        return 'c'     
+        return 'c' #Otherwises return c.      
     #Scenario #2: First Turn Betray
     if len(my_history)==0: #It's the first round; betray.
         return 'b'
-    elif my_history[-1]=='b' and their_history[-1]=='c':
+    elif my_history[-1]=='b' and their_history[-1]=='c': #If my history is b and their history is c, then return with c. 
         return 'c' 
     else:
-        return 'b' 
+        return 'b' #Overwise return b. 
     #Scenario #3:  If previous turn was c, then return b if score is -1.                                    
     """ Alternate 'c' and 'b' """
-    if my_history[-1] == 'c':
+    if my_history[-1] == 'c': 
         return 'b'
     else:
         return 'c'
@@ -38,51 +38,3 @@ def move(my_history, their_history, my_score, their_score):
     else:
         return 'b'
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
-    
-def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
-    '''
-    real_result = move(my_history, their_history, my_score, their_score)
-    if real_result == result:
-        return True
-    else:
-        print("move(" +
-            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
-                       str(my_score), str(their_score)])+
-            ") returned " + "'" + real_result + "'" +
-            " and should have returned '" + result + "'")
-        return False
-
-if __name__ == '__main__':
-     
-    # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
-              my_score=0,
-              their_score=0,
-              result='b'):
-         print 'Test passed'
-     # Test 2: Continue betraying if they collude despite being betrayed.
-    test_move(my_history='bbb',
-              their_history='ccc', 
-              # Note the scores are for testing move().
-              # The history and scores don't need to match unless
-              # that is relevant to the test of move(). Here,
-              # the simulation (if working correctly) would have awarded 
-              # 300 to me and -750 to them. This test will pass if and only if
-              # move('bbb', 'ccc', 0, 0) returns 'b'.
-              my_score=0, 
-              their_score=0,
-              result='b')             
