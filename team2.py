981ac1ea20b3f697fa86a33e4ad14777154a38e2
KeatonCooper <57014617+KeatonCooper@users.noreply.github.com>
2019-11-21 14:09:19 -0800

    Final Code team2

--- team2.py
+++ team2.py
@@ -21,16 +21,37 @@ def move(my_history, their_history, my_score, their_score):
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
+    # The most recent round is my_history[-1] andef move(my_history, their_history, my_score, their_score):
+    ''' Arguments accepted: my_history, their_history are strings.
+    my_score, their_score are ints.
+    
+    Make my move.
+    Returns 'c' or 'b'. 
+    '''
+
+    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
+    # their_history: a string of the same length as history, possibly empty. 
+    # The first round between these two players is my_history[0] and their_history[0].
     # The most recent round is my_history[-1] and their_history[-1].
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
- if len(my_history) == 0 :
+   if len(my_history) == 0 :
+       return 'c'
+   elif my_score > their_score:
+       return 'c'
+    else:
         return 'b'
+    #scenarios 1-3
     
-    return 'c'
+   
+ 
+    
+    # Analyze my_history and their_history and/or my_score and their_score.
+    # Decide whether to return 'c' or 'b'.
+    
+  
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
